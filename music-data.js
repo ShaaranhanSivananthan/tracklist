0aeb6d915d04991dc587d0c1c9ae9dcaf832abe7
ShaaranhanSivananthan <59632904+ShaaranhanSivananthan@users.noreply.github.com>
2026-09-17 11:40:07 -0400

    Update music-data.js

--- music-data.js
+++ music-data.js
@@ -1,1 +1,458 @@
+const musicData = [
 
+    // ============================================================
+    // KANYE WEST
+    // ============================================================
+
+    {
+        name: "Kanye West",
+        image: "images/artists/kanye.jpg",
+
+        albums: [
+
+            {
+                name: "The College Dropout",
+                year: 2004,
+                cover: "images/albums/tcd.jpg",
+
+                theme: {
+                    background: "#D9C29C",
+                    text: "#17130E",
+                    accent: "#8A5A24"
+                },
+
+                tracks: [
+                    "Intro",
+                    "We Don't Care",
+                    "Graduation Day",
+                    "All Falls Down",
+                    "I'll Fly Away",
+                    "Spaceship",
+                    "Jesus Walks",
+                    "Never Let Me Down",
+                    "Get Em High",
+                    "Workout Plan",
+                    "The New Workout Plan",
+                    "Slow Jamz",
+                    "Breathe in Breathe Out",
+                    "School Spirit Skit 1",
+                    "School Spirit",
+                    "School Spirit Skit 2",
+                    "Lil Jimmy Skit",
+                    "Two Words",
+                    "Through the Wire",
+                    "Family Business",
+                    "Last Call"
+                ]
+            },
+
+            {
+                name: "Late Registration",
+                year: 2005,
+                cover: "images/albums/lr.jpg",
+
+                theme: {
+                    background: "#B7C9D6",
+                    text: "#101820",
+                    accent: "#31556D"
+                },
+
+                tracks: [
+                    "Wake Up Mr. West",
+                    "Heard 'Em Say",
+                    "Touch the Sky",
+                    "Gold Digger",
+                    "Skit #1",
+                    "Drive Slow",
+                    "My Way Home",
+                    "Crack Music",
+                    "Roses",
+                    "Bring Me Down",
+                    "Addiction",
+                    "Skit #2",
+                    "Diamonds From Sierra Leone (Remix)",
+                    "We Major",
+                    "Skit #3",
+                    "Hey Mama",
+                    "Celebration",
+                    "Skit #4",
+                    "Gone",
+                    "Diamonds From Sierra Leone"
+                ]
+            },
+
+            {
+                name: "Graduation",
+                year: 2007,
+                cover: "images/albums/grad.jpg",
+
+                theme: {
+                    background: "#6F86A8",
+                    text: "#FFFFFF",
+                    accent: "#263B5A"
+                },
+
+                tracks: [
+                    "Good Morning",
+                    "Champion",
+                    "Stronger",
+                    "I Wonder",
+                    "Good Life",
+                    "Can't Tell Me Nothing",
+                    "Barry Bonds",
+                    "Drunk and Hot Girls",
+                    "Flashing Lights",
+                    "Everything I Am",
+                    "The Glory",
+                    "Homecoming",
+                    "Big Brother"
+                ]
+            },
+
+            {
+                name: "My Beautiful Dark Twisted Fantasy",
+                year: 2010,
+                cover: "images/albums/mbdtf.jpg",
+
+                theme: {
+                    background: "#3A251D",
+                    text: "#FFFFFF",
+                    accent: "#FFD700"
+                },
+
+                tracks: [
+                    "Dark Fantasy",
+                    "Gorgeous",
+                    "POWER",
+                    "All of the Lights (Interlude)",
+                    "All of the Lights",
+                    "Monster",
+                    "So Appalled",
+                    "Devil in a New Dress",
+                    "Runaway",
+                    "Hell of a Life",
+                    "Blame Game",
+                    "Lost in the World",
+                    "Who Will Survive in America"
+                ]
+            },
+
+            {
+                name: "Yeezus",
+                year: 2013,
+                cover: "images/albums/yeezus.jpg",
+
+                theme: {
+                    background: "#222222",
+                    text: "#FFFFFF",
+                    accent: "#FFFFFF"
+                },
+
+                tracks: [
+                    "On Sight",
+                    "Black Skinhead",
+                    "I Am a God",
+                    "New Slaves",
+                    "Hold My Liquor",
+                    "I'm in It",
+                    "Blood on the Leaves",
+                    "Guilt Trip",
+                    "Send It Up",
+                    "Bound 2"
+                ]
+            }
+        ]
+    },
+
+
+    // ============================================================
+    // KENDRICK LAMAR
+    // ============================================================
+
+    {
+        name: "Kendrick Lamar",
+        image: "images/artists/kendrick.jpg",
+
+        albums: [
+
+            {
+                name: "good kid, m.A.A.d city",
+                year: 2012,
+                cover: "images/albums/gkmc.jpg",
+
+                theme: {
+                    background: "#C7A77A",
+                    text: "#17120C",
+                    accent: "#714B2A"
+                },
+
+                tracks: [
+                    "Sherane a.k.a Master Splinter's Daughter",
+                    "Bitch, Don't Kill My Vibe",
+                    "Backseat Freestyle",
+                    "The Art of Peer Pressure",
+                    "Money Trees",
+                    "Poetic Justice",
+                    "good kid",
+                    "m.A.A.d city",
+                    "Swimming Pools (Drank)",
+                    "Sing About Me, I'm Dying of Thirst",
+                    "Real",
+                    "Compton"
+                ]
+            },
+
+            {
+                name: "To Pimp a Butterfly",
+                year: 2015,
+                cover: "images/albums/tpab.jpg",
+
+                theme: {
+                    background: "#4C3127",
+                    text: "#FFFFFF",
+                    accent: "#C98B4A"
+                },
+
+                tracks: [
+                    "Wesley's Theory",
+                    "For Free? (Interlude)",
+                    "King Kunta",
+                    "Institutionalized",
+                    "These Walls",
+                    "u",
+                    "Alright",
+                    "For Sale? (Interlude)",
+                    "Momma",
+                    "Hood Politics",
+                    "How Much a Dollar Cost",
+                    "Complexion (A Zulu Love)",
+                    "The Blacker the Berry",
+                    "You Ain't Gotta Lie (Momma Said)",
+                    "i",
+                    "Mortal Man"
+                ]
+            },
+
+            {
+                name: "DAMN.",
+                year: 2017,
+                cover: "images/albums/damn.jpg",
+
+                theme: {
+                    background: "#9A1515",
+                    text: "#FFFFFF",
+                    accent: "#111111"
+                },
+
+                tracks: [
+                    "BLOOD.",
+                    "DNA.",
+                    "YAH.",
+                    "ELEMENT.",
+                    "FEEL.",
+                    "LOYALTY. FEAT. RIHANNA",
+                    "PRIDE.",
+                    "HUMBLE.",
+                    "LUST.",
+                    "LOVE. FEAT. ZACARI",
+                    "XXX. FEAT. U2",
+                    "FEAR.",
+                    "GOD.",
+                    "DUCKWORTH."
+                ]
+            },
+
+            {
+                name: "Mr. Morale & the Big Steppers",
+                year: 2022,
+                cover: "images/albums/mr&bs.jpg",
+
+                theme: {
+                    background: "#D6C4A5",
+                    text: "#19140E",
+                    accent: "#624F3A"
+                },
+
+                tracks: [
+                    "United in Grief",
+                    "N95",
+                    "Worldwide Steppers",
+                    "Die Hard",
+                    "Father Time",
+                    "Rich (Interlude)",
+                    "Rich Spirit",
+                    "We Cry Together",
+                    "Purple Hearts",
+                    "Count Me Out",
+                    "Crown",
+                    "Silent Hill",
+                    "Savior (Interlude)",
+                    "Savior",
+                    "Auntie Diaries",
+                    "Mr. Morale",
+                    "Mother I Sober",
+                    "Mirror"
+                ]
+            }
+        ]
+    },
+
+
+    // ============================================================
+    // THE WEEKND
+    // ============================================================
+
+    {
+        name: "The Weeknd",
+        image: "images/artists/weeknd.jpg",
+
+        albums: [
+
+            {
+                name: "House of Balloons",
+                year: 2011,
+                cover: "images/albums/hob.jpg",
+
+                theme: {
+                    background: "#17151B",
+                    text: "#FFFFFF",
+                    accent: "#8C62A8"
+                },
+
+                tracks: [
+                    "High for This",
+                    "What You Need",
+                    "House of Balloons / Glass Table Girls",
+                    "The Morning",
+                    "Wicked Games",
+                    "The Party & the After Party",
+                    "Coming Down",
+                    "Loft Music",
+                    "The Knowing"
+                ]
+            },
+
+            {
+                name: "Beauty Behind the Madness",
+                year: 2015,
+                cover: "images/albums/bbtm.jpg",
+
+                theme: {
+                    background: "#A89B87",
+                    text: "#16130F",
+                    accent: "#5A4A3A"
+                },
+
+                tracks: [
+                    "Real Life",
+                    "Losers",
+                    "Tell Your Friends",
+                    "Often",
+                    "The Hills",
+                    "Acquainted",
+                    "Can't Feel My Face",
+                    "Shameless",
+                    "Earned It",
+                    "In the Night",
+                    "As You Are",
+                    "Dark Times",
+                    "Prisoner",
+                    "Angel"
+                ]
+            },
+
+            {
+                name: "Starboy",
+                year: 2016,
+                cover: "images/albums/starboy.jpg",
+
+                theme: {
+                    background: "#111111",
+                    text: "#FFFFFF",
+                    accent: "#C6A15B"
+                },
+
+                tracks: [
+                    "Starboy",
+                    "Party Monster",
+                    "False Alarm",
+                    "Reminder",
+                    "Rockin'",
+                    "Secrets",
+                    "True Colors",
+                    "Stargirl Interlude",
+                    "Sidewalks",
+                    "Six Feet Under",
+                    "Love to Lay",
+                    "A Lonely Night",
+                    "Attention",
+                    "Ordinary Life",
+                    "Nothing Without You",
+                    "All I Know",
+                    "Die for You",
+                    "I Feel It Coming"
+                ]
+            },
+
+            {
+                name: "After Hours",
+                year: 2020,
+                cover: "images/albums/ah.jpg",
+
+                theme: {
+                    background: "#8C1D25",
+                    text: "#FFFFFF",
+                    accent: "#2C1114"
+                },
+
+                tracks: [
+                    "Alone Again",
+                    "Too Late",
+                    "Hardest to Love",
+                    "Scared to Live",
+                    "Snowchild",
+                    "Escape from LA",
+                    "Heartless",
+                    "Faith",
+                    "Blinding Lights",
+                    "In Your Eyes",
+                    "Save Your Tears",
+                    "Repeat After Me (Interlude)",
+                    "After Hours",
+                    "Until I Bleed Out"
+                ]
+            },
+
+            {
+                name: "Dawn FM",
+                year: 2022,
+                cover: "images/albums/dfm.jpg",
+
+                theme: {
+                    background: "#B7B7B7",
+                    text: "#151515",
+                    accent: "#555555"
+                },
+
+                tracks: [
+                    "Dawn FM",
+                    "Gasoline",
+                    "How Do I Make You Love Me?",
+                    "Take My Breath",
+                    "Sacrifice",
+                    "A Tale by Quincy",
+                    "Out of Time",
+                    "Here We Go... Again",
+                    "Best Friends",
+                    "Is There Someone Else?",
+                    "Starry Eyes",
+                    "Every Angel Is Terrifying",
+                    "Don't Break My Heart",
+                    "I Heard You're Married",
+                    "Less Than Zero",
+                    "Phantom Regret by Jim"
+                ]
+            }
+        ]
+    }
+];
